eaa9ab9822d90cc9afb381de4fe51f8200078279
nandiniperikala <nandini.p@comakeit.com>
2023-11-08 15:36:42 +0530

    ingress files removed

--- generator-tekton/generators/app/assets/filesList.js
+++ generator-tekton/generators/app/assets/filesList.js
@@ -1,6 +1,6 @@
 const fileListjibpipeline = [
   "jib/pipeline/00-namespace.yml",
-  "jib/pipeline/01-ssh-credentials.yml",
+  "jib/pipeline/01-secrets.yml",
   "jib/pipeline/02-rbac.yml",
   "jib/pipeline/03-pipeline.yml",
   "jib/pipeline/04-pipelinerun.yml",
@@ -9,34 +9,30 @@ const fileListjibpipeline = [
 
 const fileListjibtriggers = [
   "jib/triggers/00-namespace.yml",
-  "jib/triggers/01-ssh-credentials.yml",
+  "jib/triggers/01-secrets.yml",
   "jib/triggers/02-rbac.yml",
   "jib/triggers/03-pipeline.yml",
   "jib/triggers/04-event-listener.yml",
-  "jib/triggers/05-ingress-event.yml",
-  "jib/triggers/06-triggers.yml",
+  "jib/triggers/05-triggers.yml",
   "README.md"
 ];
 
 const fileListkanikopipeline = [
   "kaniko/pipeline/00-namespace.yml",
-  "kaniko/pipeline/01-ssh-credentials.yml",
-  "kaniko/pipeline/02-docker-credentials.yml",
-  "kaniko/pipeline/03-rbac.yml",
-  "kaniko/pipeline/04-pipeline.yml",
-  "kaniko/pipeline/05-pipelinerun.yml",
+  "kaniko/pipeline/01-secrets.yml",
+  "kaniko/pipeline/02-rbac.yml",
+  "kaniko/pipeline/03-pipeline.yml",
+  "kaniko/pipeline/04-pipelinerun.yml",
   "README.md"
 ];
 
 const fileListkanikotriggers = [
   "kaniko/triggers/00-namespace.yml",
-  "kaniko/triggers/01-ssh-credentials.yml",
-  "kaniko/triggers/02-docker-credentials.yml",
-  "kaniko/triggers/03-rbac.yml",
-  "kaniko/triggers/04-pipeline.yml",
-  "kaniko/triggers/05-event-listener.yml",
-  "kaniko/triggers/06-ingress-event.yml",
-  "kaniko/triggers/07-triggers.yml",
+  "kaniko/triggers/01-secrets.yml",
+  "kaniko/triggers/02-rbac.yml",
+  "kaniko/triggers/03-pipeline.yml",
+  "kaniko/triggers/04-event-listener.yml",
+  "kaniko/triggers/05-triggers.yml",
   "README.md"
 ];
 
